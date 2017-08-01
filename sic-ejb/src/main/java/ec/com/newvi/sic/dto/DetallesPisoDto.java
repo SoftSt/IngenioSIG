519b8e304f393b07fad2cd58279f066830cefe69
Jorge Jacome <andy.2892@hotmail.com>
2017-08-01 11:29:12 -0500

    Creacion de un dto para las caracteristicas de edificacion

--- sic-ejb/src/main/java/ec/com/newvi/sic/dto/DetallesPisoDto.java
+++ sic-ejb/src/main/java/ec/com/newvi/sic/dto/DetallesPisoDto.java
@@ -7,6 +7,7 @@ package ec.com.newvi.sic.dto;
 
 import ec.com.newvi.sic.modelo.PisoDetalle;
 import ec.com.newvi.sic.modelo.Pisos;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,21 +16,94 @@ import java.util.List;
  */
 public class DetallesPisoDto {
     
-    private List<PisoDetalle> listaDetallesPiso;
-
-    public List<PisoDetalle> getListaDetallesPiso() {
-        return listaDetallesPiso;
-    }
-
-    public void setListaDetallesPiso(List<PisoDetalle> listaDetallesPiso) {
-        this.listaDetallesPiso = listaDetallesPiso;
-    }
     
+    
+    /*detalles piso*/
+    private String subgrupo;
+    private String descripcion;
+    private String estadoDetalle;
+    
+    
+    private String nomBloque;
+    private String nomPiso;
+    private String valNropisos;
+    private BigDecimal valAreapiso;
+    private Integer valAnioconstruccion;
+    private Integer valAnioreparacion;
+
+    public String getSubgrupo() {
+        return subgrupo;
+    }
+
+    public void setSubgrupo(String subgrupo) {
+        this.subgrupo = subgrupo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstadoDetalle() {
+        return estadoDetalle;
+    }
+
+    public void setEstadoDetalle(String estadoDetalle) {
+        this.estadoDetalle = estadoDetalle;
+    }
+
+    public String getNomBloque() {
+        return nomBloque;
+    }
+
+    public void setNomBloque(String nomBloque) {
+        this.nomBloque = nomBloque;
+    }
+
+    public String getNomPiso() {
+        return nomPiso;
+    }
+
+    public void setNomPiso(String nomPiso) {
+        this.nomPiso = nomPiso;
+    }
+
+    public String getValNropisos() {
+        return valNropisos;
+    }
+
+    public void setValNropisos(String valNropisos) {
+        this.valNropisos = valNropisos;
+    }
+
+    public BigDecimal getValAreapiso() {
+        return valAreapiso;
+    }
+
+    public void setValAreapiso(BigDecimal valAreapiso) {
+        this.valAreapiso = valAreapiso;
+    }
+
+    public Integer getValAnioconstruccion() {
+        return valAnioconstruccion;
+    }
+
+    public void setValAnioconstruccion(Integer valAnioconstruccion) {
+        this.valAnioconstruccion = valAnioconstruccion;
+    }
+
+    public Integer getValAnioreparacion() {
+        return valAnioreparacion;
+    }
+
+    public void setValAnioreparacion(Integer valAnioreparacion) {
+        this.valAnioreparacion = valAnioreparacion;
+    }
 
     public DetallesPisoDto() {
-    }
-    public DetallesPisoDto(Pisos piso) {
-        this.listaDetallesPiso = (List<PisoDetalle>)piso.getDetalles();
     }
     
     
