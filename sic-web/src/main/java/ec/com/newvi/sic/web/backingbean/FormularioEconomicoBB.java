b3034be69427dba9c6eeb6c40b6e84a288ffff2c
Jorge Jacome <andy.2892@hotmail.com>
2017-08-18 15:22:28 -0500

    formularios economicos

---
+++ sic-web/src/main/java/ec/com/newvi/sic/web/backingbean/FormularioEconomicoBB.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.newvi.sic.web.backingbean;
+
+import ec.com.newvi.sic.dto.FichaCatastralDto;
+import ec.com.newvi.sic.modelo.Predios;
+import ec.com.newvi.sic.util.excepciones.NewviExcepcion;
+import ec.com.newvi.sic.util.logs.LoggerNewvi;
+import ec.com.newvi.sic.web.MensajesFaces;
+import ec.com.newvi.sic.web.enums.EnumEtiquetas;
+import ec.com.newvi.sic.web.enums.EnumPantallaMantenimiento;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Andrés
+ */
+@ManagedBean
+@ViewScoped
+
+public class FormularioEconomicoBB extends AdminFichaCatastralBB {
+
+    private List<FichaCatastralDto> listaFichas;
+    private List<FichaCatastralDto> listaFichasFiltradas;
+    private Predios predio;
+    private EnumPantallaMantenimiento pantallaActual;
+
+    public EnumPantallaMantenimiento getPantallaActual() {
+        return pantallaActual;
+    }
+
+    public void setPantallaActual(EnumPantallaMantenimiento pantallaActual) {
+        this.pantallaActual = pantallaActual;
+    }
+
+    public Predios getPredio() {
+        return predio;
+    }
+
+    public void setPredio(Predios predio) {
+        this.predio = predio;
+    }
+
+    public List<FichaCatastralDto> getListaFichas() {
+        return listaFichas;
+    }
+
+    public void setListaFichas(List<FichaCatastralDto> listaFichas) {
+        this.listaFichas = listaFichas;
+    }
+
+    public List<FichaCatastralDto> getListaFichasFiltradas() {
+        return listaFichasFiltradas;
+    }
+
+    public void setListaFichasFiltradas(List<FichaCatastralDto> listaFichasFiltradas) {
+        this.listaFichasFiltradas = listaFichasFiltradas;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.predio = new Predios();
+
+        actualizarListadoPredios();
+
+        conmutarPantalla(EnumPantallaMantenimiento.PANTALLA_LISTADO);
+        establecerTitulo(EnumEtiquetas.FICHA_CATASTRAL_LISTA_TITULO,
+                EnumEtiquetas.FICHA_CATASTRAL_LISTA_ICONO,
+                EnumEtiquetas.FICHA_CATASTRAL_LISTA_DESCRIPCION);
+
+    }
+
+    private void actualizarListadoPredios() {
+        List<Predios> listaPredios = catastroServicio.consultarPredios();
+        listaFichas = new ArrayList<>();
+        listaPredios.forEach((elementoPredio) -> {
+            listaFichas.add(new FichaCatastralDto(elementoPredio));
+        });
+    }
+
+    private void conmutarPantalla(EnumPantallaMantenimiento nuevaPantalla) {
+        this.pantallaActual = nuevaPantalla;
+    }
+    
+    public Boolean esPantallaActual(String pantallaEsperada) {
+        try {
+            return this.pantallaActual.equals(EnumPantallaMantenimiento.obtenerPantallaPorNombre(pantallaEsperada));
+        } catch (NewviExcepcion e) {
+            LoggerNewvi.getLogNewvi(this.getClass()).error(e, sesionBean.obtenerSesionDto());
+            MensajesFaces.mensajeError(e.getMessage());
+            return false;
+        }
+    }
+
+}
